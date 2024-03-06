d382ed4d5fa276be56ca9fd7d7dd625293414a75
Lydien Mamitiana <mamitiana22.aps2a@gmail.com>
2024-03-06 12:39:18 +0200

    voice command

--- lib/home.dart
+++ lib/home.dart
@@ -1,106 +1,126 @@
-// ignore_for_file: unused_field
+// ignore_for_file: library_private_types_in_public_api
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'dart:async';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  static const apiKey = "AIzaSyCzjSwgYzcjoiVDQO76KbFvKbKJVTtnZjs";
+  static const String apiKey = "AIzaSyBSq6zwXD7nX2xsuZFUOUWzH7Q3ep9ROl8";
   final model = GenerativeModel(model: 'gemini-1.0-pro', apiKey: apiKey);
-  final TextEditingController _userMessage = TextEditingController();
-  final List<Message> _messages = [];
-  final bool _isMessageEmpty = true;
+  final stt.SpeechToText speech = stt.SpeechToText();
+  final FlutterTts flutterTts = FlutterTts();
+  String _text = '';
+  bool _isListening = false;
+  late Timer _timer;
+  bool _isSpeaking = false;
 
-  Future<void> sendMessage() async {
-    final message = _userMessage.text
-        .trim(); // Trim pour supprimer les espaces vides au début et à la fin
-    if (message.isEmpty) {
-      // Si le message est vide, ne rien faire
-      return;
-    }
+  // Contrôleur pour l'animation du modèle 3D
+  late ModelViewer modelViewer;
 
-    _userMessage.clear();
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+    modelViewer = const ModelViewer(
+      autoPlay: false,
+      src: 'assets/angelica.glb',
+    );
+  }
 
-    setState(() {
-      _messages.add(Message(true, message, DateTime.now()));
-    });
+  void _initSpeech() async {
+    // Demande d'autorisation d'accès au microphone
+    await speech.initialize(
+      onStatus: (status) {
+        if (kDebugMode) {
+          print('Speech recognition status: $status');
+        }
+      },
+      onError: (error) {
+        if (kDebugMode) {
+          print('Speech recognition error: $error');
+        }
+      },
+    );
 
-    final content = [Content.text(message)];
-    final response = await model.generateContent(content);
+    //bool isAvailable = await speech.requestPermission();
 
-    setState(() {
-      _messages.add(Message(false, response.text ?? "", DateTime.now()));
-    });
+    // if (!isAvailable) {
+    //   if (kDebugMode) {
+    //     print('Speech recognition not available');
+    //   }
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gemini Chat Bot'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
+      body: Stack(
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                final message = _messages[index];
-                return ListTile(
-                  title: Text(message.message),
-                  subtitle: Text(DateFormat('HH:mm').format(message.date)),
-                  trailing: message.isUser
-                      ? const Icon(Icons.person)
-                      : const Icon(Icons.chat_bubble_outline),
-                  tileColor:
-                      message.isUser ? Colors.grey[300] : Colors.blue[100],
-                );
-              },
-            ),
+          Image.asset(
+            'assets/background.jpg',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
           ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: TextFormField(
-                        controller: _userMessage,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(50)),
-                          ),
-                          labelText: 'Enter your message',
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              // Dissimuler le clavier lors du clic sur le bouton
-                              FocusScope.of(context).unfocus();
-                            },
-                            icon: const Icon(Icons.keyboard_arrow_down),
-                          ),
-                        ),
-                        maxLines:
-                            null, // Permet à TextFormField de se mettre à la ligne automatiquement
-                        textAlignVertical: TextAlignVertical
-                            .top, // Alignement du texte en haut
-                      ),
+                SizedBox(
+                  width: 400,
+                  height: 400,
+                  child: modelViewer,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onLongPress: _startListening,
+                  onLongPressEnd: (_) => _stopListening(),
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(70),
                     ),
+                    child: Icon(_isListening ? Icons.mic_off : Icons.mic),
                   ),
                 ),
-                IconButton(
-                  onPressed: sendMessage,
-                  icon: const Icon(Icons.send),
-                  color: Colors.blue,
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.white), // Fond blanc
+                    foregroundColor:
+                        MaterialStateProperty.all(Colors.black), // Texte noir
+                  ),
+                  onPressed: _stopSpeaking,
+                  child: const Text(
+                    'Stop Speaking',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ), // Couleur du texte noire
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  _text,
+                  style: const TextStyle(color: Colors.white),
                 ),
               ],
             ),
@@ -109,55 +129,97 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-}
 
-class Messages extends StatelessWidget {
-  final bool isUser;
-  final String message;
-  final String date;
-  const Messages(
-      {super.key,
-      required this.isUser,
-      required this.message,
-      required this.date});
+  void _startListening() async {
+    setState(() {
+      _isListening = true;
+      _text = 'Listening...';
+    });
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(15),
-      margin: const EdgeInsets.symmetric(vertical: 15)
-          .copyWith(left: isUser ? 100 : 10, right: isUser ? 10 : 100),
-      decoration: BoxDecoration(
-          color: isUser
-              ? const Color.fromARGB(255, 9, 48, 79)
-              : Colors.grey.shade300,
-          borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(10),
-              bottomLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-              bottomRight: Radius.circular(10))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            message,
-            style: TextStyle(color: isUser ? Colors.white : Colors.black),
-          ),
-          Text(
-            date,
-            style: TextStyle(color: isUser ? Colors.white : Colors.black),
-          )
-        ],
-      ),
+    bool isAvailable = await speech.listen(
+      onResult: (result) async {
+        setState(() {
+          _text = result.recognizedWords;
+        });
+
+        // Log pour afficher le texte reconnu avant nettoyage
+        if (kDebugMode) {
+          print('Text before cleaning: ${result.recognizedWords}');
+        }
+
+        // Supprimer les caractères spéciaux des réponses vocales
+        String cleanedText = _cleanText(result.recognizedWords);
+
+        // Log pour afficher le texte après nettoyage
+        if (kDebugMode) {
+          print('Text after cleaning: $cleanedText');
+        }
+
+        // Envoyer la question à l'API de Gemini et obtenir la réponse
+        GenerateContentResponse geminiResponse =
+            await getGeminiResponse(cleanedText);
+
+        // Reproduire la réponse vocalement seulement si elle n'est pas en cours de lecture
+        if (!_isSpeaking) {
+          _isSpeaking = true;
+          await _speak(geminiResponse.text ?? "");
+          _isSpeaking = false;
+        }
+
+        // Arrêter d'écouter après quelques secondes
+        _timer = Timer(const Duration(seconds: 2), () {
+          _stopListening();
+        });
+      },
     );
+
+    if (!isAvailable) {
+      setState(() {
+        _isListening = false;
+        _text = 'Speech recognition not available';
+      });
+    }
   }
-}
 
-class Message {
-  final bool isUser;
-  final String message;
-  final DateTime date;
+  // String _cleanText(String text) {
+  //   // Utiliser une expression régulière pour supprimer les caractères spéciaux
+  //   return text.replaceAll(RegExp(r'[^\w\s]+'), '');
+  // }
+  String _cleanText(String text) {
+    // Convertir le texte en minuscules et supprimer les caractères non alphanumériques
+    return text.toLowerCase().replaceAll(RegExp(r'[^a-z0-9\s]+'), '');
+  }
 
-  Message(this.isUser, this.message, this.date);
+  Future<GenerateContentResponse> getGeminiResponse(String question) async {
+    // Generate content using the model
+    try {
+      final response = await model.generateContent([Content.text(question)]);
+      return response;
+    } catch (e) {
+      // Handle error
+      if (kDebugMode) {
+        print('Error generating response from Gemini: $e');
+      }
+      throw 'An error occurred. Please try again.';
+    }
+  }
+
+  Future<void> _speak(String text) async {
+    // Utilisez FlutterTts pour reproduire le texte vocalement
+    await flutterTts.speak(text);
+  }
+
+  void _stopListening() {
+    if (speech.isListening) {
+      setState(() {
+        _isListening = false;
+      });
+      speech.stop();
+      _timer.cancel(); // Annuler le timer si l'arrêt est manuel
+    }
+  }
+
+  void _stopSpeaking() {
+    flutterTts.stop();
+  }
 }
